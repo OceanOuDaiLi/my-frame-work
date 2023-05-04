74cc122a175b3c84b35439db4d9411e3bbba688d
kevin.ou <877859249@qq.com>
2023-05-04 20:55:44 +0800

    Update BundleDetailList.cs

--- EngineFrameWork/Assets/Editor/AssetBuilder/AssetBundleBrowser/BundleDetailList.cs
+++ EngineFrameWork/Assets/Editor/AssetBuilder/AssetBundleBrowser/BundleDetailList.cs
@@ -20,7 +20,7 @@ namespace AssetBundleBrowser
     internal class TogglePathTreeViewItem : TreeViewItem
     {
         private static bool m_DisplayAlt = false;
-        
+
         private string m_DisplayNamePrefix;
         private string m_Path;
 
@@ -28,13 +28,13 @@ namespace AssetBundleBrowser
         {
             get { return m_Path; }
         }
-        
+
         public string DisplayNamePrefix
         {
             get { return m_DisplayNamePrefix; }
         }
 
-        public TogglePathTreeViewItem( int id, int depth, string displayName, string path )
+        public TogglePathTreeViewItem(int id, int depth, string displayName, string path)
         {
             base.depth = depth;
             base.id = id;
@@ -42,8 +42,8 @@ namespace AssetBundleBrowser
             m_Path = path;
             m_DisplayNamePrefix = "";
         }
-        
-        public TogglePathTreeViewItem( int id, int depth, string displayNamePrefix, string displayName, string path )
+
+        public TogglePathTreeViewItem(int id, int depth, string displayNamePrefix, string displayName, string path)
         {
             base.depth = depth;
             base.id = id;
@@ -51,17 +51,17 @@ namespace AssetBundleBrowser
             m_Path = path;
             m_DisplayNamePrefix = displayNamePrefix;
         }
-        
+
         public override string displayName
         {
             get
             {
                 // TODO this is a bit unresponsive here in large projects, see if can be better elsewhere
                 Event e = Event.current;
-                if( e.alt && e.type == EventType.MouseDown )
+                if (e.alt && e.type == EventType.MouseDown)
                     m_DisplayAlt = !m_DisplayAlt;
 
-                return m_DisplayNamePrefix + ( m_DisplayAlt ? m_Path : base.displayName );
+                return m_DisplayNamePrefix + (m_DisplayAlt ? m_Path : base.displayName);
             }
             set
             {
@@ -98,7 +98,7 @@ namespace AssetBundleBrowser
             if (dirty)
             {
                 Reload();
-                ExpandAll( 2 );
+                ExpandAll(2);
             }
         }
         protected override TreeViewItem BuildRoot()
@@ -107,7 +107,7 @@ namespace AssetBundleBrowser
             root.children = new List<TreeViewItem>();
             if (m_Selecteditems != null)
             {
-                foreach(var bundle in m_Selecteditems)
+                foreach (var bundle in m_Selecteditems)
                 {
                     root.AddChild(AppendBundleToTree(bundle));
                 }
@@ -120,7 +120,7 @@ namespace AssetBundleBrowser
             if ((args.item as BundleDetailItem) != null)
             {
                 EditorGUI.HelpBox(
-                    new Rect(args.rowRect.x + k_DoubleIndent, args.rowRect.y, args.rowRect.width - k_DoubleIndent, args.rowRect.height), 
+                    new Rect(args.rowRect.x + k_DoubleIndent, args.rowRect.y, args.rowRect.width - k_DoubleIndent, args.rowRect.height),
                     args.item.displayName,
                     (args.item as BundleDetailItem).MessageLevel);
             }
@@ -141,7 +141,7 @@ namespace AssetBundleBrowser
         }
         protected override float GetCustomRowHeight(int row, TreeViewItem item)
         {
-            if( (item as BundleDetailItem) != null)
+            if ((item as BundleDetailItem) != null)
             {
                 float height = DefaultStyles.backgroundEven.CalcHeight(new GUIContent(item.displayName), m_TotalRect.width);
                 return height + 3f;
@@ -149,56 +149,56 @@ namespace AssetBundleBrowser
             return base.GetCustomRowHeight(row, item);
         }
 
-        
-        protected override void SelectionChanged( IList<int> selectedIds )
+
+        protected override void SelectionChanged(IList<int> selectedIds)
         {
-            base.SelectionChanged( selectedIds );
+            base.SelectionChanged(selectedIds);
             List<string> pathList = new List<string>();
 
-            for( int i = 0; i < selectedIds.Count; ++i )
+            for (int i = 0; i < selectedIds.Count; ++i)
             {
-                TreeViewItem item = this.FindItem( selectedIds[i], rootItem );
-                if( item != null )
+                TreeViewItem item = this.FindItem(selectedIds[i], rootItem);
+                if (item != null)
                 {
-                    AddDependentAssetsRecursive( item, pathList );
+                    AddDependentAssetsRecursive(item, pathList);
                 }
             }
-            
-            AssetBundleBrowserMain.instance.m_ManageTab.SetAssetListSelection( pathList );
+
+            AssetBundleBrowserMain.instance.m_ManageTab.SetAssetListSelection(pathList);
         }
 
-        void AddDependentAssetsRecursive( TreeViewItem item, List<string> pathList )
+        void AddDependentAssetsRecursive(TreeViewItem item, List<string> pathList)
         {
             TogglePathTreeViewItem pathItem = item as TogglePathTreeViewItem;
-            if( pathItem != null )
+            if (pathItem != null)
             {
-                if( string.IsNullOrEmpty(pathItem.DisplayNamePrefix) == false && pathList.Contains( pathItem.Path ) == false )
+                if (string.IsNullOrEmpty(pathItem.DisplayNamePrefix) == false && pathList.Contains(pathItem.Path) == false)
                 {
-                    pathList.Add( pathItem.Path );
+                    pathList.Add(pathItem.Path);
                 }
             }
 
-            if( item.hasChildren )
+            if (item.hasChildren)
             {
-                for( int i=0; i<item.children.Count; ++i )
-                    AddDependentAssetsRecursive( item.children[i], pathList );
+                for (int i = 0; i < item.children.Count; ++i)
+                    AddDependentAssetsRecursive(item.children[i], pathList);
             }
         }
 
-        protected override void DoubleClickedItem( int id )
+        protected override void DoubleClickedItem(int id)
         {
-            base.DoubleClickedItem( id );
-            TreeViewItem item = this.FindItem( id, rootItem );
-            if( item != null )
+            base.DoubleClickedItem(id);
+            TreeViewItem item = this.FindItem(id, rootItem);
+            if (item != null)
             {
                 TogglePathTreeViewItem pathItem = item as TogglePathTreeViewItem;
-                if( pathItem != null )
+                if (pathItem != null)
                 {
-                    Object o = AssetDatabase.LoadAssetAtPath<Object>( pathItem.Path );
-                    if( o != null )
+                    Object o = AssetDatabase.LoadAssetAtPath<Object>(pathItem.Path);
+                    if (o != null)
                     {
                         Selection.activeObject = o;
-                        EditorGUIUtility.PingObject( o );
+                        EditorGUIUtility.PingObject(o);
                     }
                 }
             }
@@ -215,36 +215,36 @@ namespace AssetBundleBrowser
             str = itemName + k_DependencyHeader;
             var dependency = new TreeViewItem(str.GetHashCode(), 1, k_DependencyEmpty);
             var depList = bundle.GetBundleDependencies();
-            if(depList.Count > 0)
+            if (depList.Count > 0)
             {
                 dependency.displayName = k_DependencyHeader;
                 foreach (var dep in bundle.GetBundleDependencies())
                 {
                     str = itemName + dep.m_BundleName;
-                    TreeViewItem newItem = new TreeViewItem( str.GetHashCode(), 2, dep.m_BundleName );
-                    newItem.icon = Model.GetBundleIcon();
+                    TreeViewItem newItem = new TreeViewItem(str.GetHashCode(), 2, dep.m_BundleName);
+                    newItem.icon = AssetBundleBrowser.AssetBundleModel.Model.GetBundleIcon();
                     dependency.AddChild(newItem);
-                    
+
                     Dictionary<string, TogglePathTreeViewItem> toAssetItems = new Dictionary<string, TogglePathTreeViewItem>();
 
-                    for( int i = 0; i < dep.m_FromAssets.Count; ++i )
+                    for (int i = 0; i < dep.m_FromAssets.Count; ++i)
                     {
                         TogglePathTreeViewItem item = null;
-                        
-                        if( ! toAssetItems.TryGetValue( dep.m_ToAssets[i].fullAssetName, out item ) )
+
+                        if (!toAssetItems.TryGetValue(dep.m_ToAssets[i].fullAssetName, out item))
                         {
                             str = itemName + dep.m_BundleName + dep.m_ToAssets[i].displayName;
-                            item = new TogglePathTreeViewItem( str.GetHashCode(), 3, "/"+dep.m_ToAssets[i].displayName, "/"+dep.m_ToAssets[i].fullAssetName );
+                            item = new TogglePathTreeViewItem(str.GetHashCode(), 3, "/" + dep.m_ToAssets[i].displayName, "/" + dep.m_ToAssets[i].fullAssetName);
                             item.icon = AssetDatabase.GetCachedIcon(dep.m_ToAssets[i].fullAssetName) as Texture2D;
-                            newItem.AddChild( item );
-                            toAssetItems.Add( dep.m_ToAssets[i].fullAssetName, item );
+                            newItem.AddChild(item);
+                            toAssetItems.Add(dep.m_ToAssets[i].fullAssetName, item);
                         }
 
                         str = str + dep.m_FromAssets[i].displayName;
-                        TreeViewItem refItem = new TogglePathTreeViewItem( str.GetHashCode(), 4, k_ReferencedPrefix,
-                            dep.m_FromAssets[i].displayName, dep.m_FromAssets[i].fullAssetName );
+                        TreeViewItem refItem = new TogglePathTreeViewItem(str.GetHashCode(), 4, k_ReferencedPrefix,
+                            dep.m_FromAssets[i].displayName, dep.m_FromAssets[i].fullAssetName);
                         refItem.icon = AssetDatabase.GetCachedIcon(dep.m_FromAssets[i].fullAssetName) as Texture2D;
-                        item.AddChild( refItem );
+                        item.AddChild(refItem);
                     }
                 }
             }
@@ -256,7 +256,7 @@ namespace AssetBundleBrowser
                 msg.displayName = k_MessageHeader;
                 var currMessages = bundle.GetMessages();
 
-                foreach(var currMsg in currMessages)
+                foreach (var currMsg in currMessages)
                 {
                     str = itemName + currMsg.message;
                     msg.AddChild(new BundleDetailItem(str.GetHashCode(), 2, currMsg.message, currMsg.severity));
@@ -276,13 +276,13 @@ namespace AssetBundleBrowser
         internal void SetItems(IEnumerable<AssetBundleModel.BundleInfo> items)
         {
             m_Selecteditems.Clear();
-            foreach(var item in items)
+            foreach (var item in items)
             {
                 CollectBundles(item);
             }
             SetSelection(new List<int>());
             Reload();
-            ExpandAll( 2 );
+            ExpandAll(2);
         }
         internal void CollectBundles(AssetBundleModel.BundleInfo bundle)
         {
@@ -299,22 +299,22 @@ namespace AssetBundleBrowser
             }
         }
 
-        internal void ExpandAll( int maximumDepth )
+        internal void ExpandAll(int maximumDepth)
         {
-            List<int> expanded = new List<int>( GetExpanded() );
-            FindItems( rootItem, maximumDepth, expanded );
-            SetExpanded( expanded );
+            List<int> expanded = new List<int>(GetExpanded());
+            FindItems(rootItem, maximumDepth, expanded);
+            SetExpanded(expanded);
         }
-        
-        internal void FindItems( TreeViewItem item, int maximumDepth, List<int> expanded )
+
+        internal void FindItems(TreeViewItem item, int maximumDepth, List<int> expanded)
         {
-            if( item.depth >= maximumDepth || ! item.hasChildren )
+            if (item.depth >= maximumDepth || !item.hasChildren)
                 return;
-            
-            expanded.Add( item.id );
-            for( int i = 0; i < item.children.Count; ++i )
+
+            expanded.Add(item.id);
+            for (int i = 0; i < item.children.Count; ++i)
             {
-                FindItems( item.children[i], maximumDepth, expanded );
+                FindItems(item.children[i], maximumDepth, expanded);
             }
         }
     }
