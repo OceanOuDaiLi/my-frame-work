09289134b6cf200db435e4ded7aa764823b5c99d
kevin.ou <877859249@qq.com>
2023-06-20 18:34:44 +0800

    Update: ETTASK ,net FW & Test load luban cfg.

---
+++ EngineFrameWork/Assets/Test.cs
@@ -0,0 +1,28 @@
+using cfg.ai;
+using FrameWork;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using System.Collections;
+using System.Text;
+
+public class Test : MonoBehaviour
+{
+    //// Start is called before the first frame update
+    //IEnumerator Start()
+    //{
+    //    GameMgr.Ins.Startup();
+    //    while (!GameMgr.Ins.Inited)
+    //    {
+    //        yield return null;
+    //    }
+    //    string path = "db/ai_tbblackboard.json";
+    //    string assetPath = App.Env.AssetPath.Substring(App.Env.AssetPath.IndexOf("Assets")) + App.Env.ResourcesNoBuildPath + Path.AltDirectorySeparatorChar + path;
+    //    var tar = AssetDatabase.LoadAssetAtPath(assetPath, typeof(TextAsset));
+
+    //    //var tar = File.ReadAllText(assetPath, System.Text.Encoding.UTF8);
+    //    //var result = App.Json.Decode<TbBlackboard>(cntent);
+    //    //TbBlackboard result = new TbBlackboard(cntent);
+    //    //Debug.Log(cntent);
+    //}
+}
